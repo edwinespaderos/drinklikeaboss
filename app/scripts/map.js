b93d0477d84b2debb9025119fb7937fd93f00357
Edwin Espaderos <eespaderos@gmail.com>
2016-11-01 13:50:58 -0400

    added finder js

--- app/scripts/map.js
+++ app/scripts/map.js
@@ -1,74 +1,95 @@
-function getQuery (postalCode) {
-  return [
-    'query {',
-      // customize the zip based on what the user is looking at
-      'locations(brand:GREAT_AMERICA, zip:' + postalCode + ') {',
-        // all the fields we care about for our UI
-        'name street city state zip lat long distance',
-      '}',
-    '}',
-  ].join('');
-}
+(function () {
+    // https://gist.github.com/padolsey/6008842
+    var makeInterpolator = (function() {
+        var rc = {'\n': '\\n', '\"': '\\\"', '\u2028': '\\u2028', '\u2029': '\\u2029'};
+        return function makeInterpolator(str) {
+            return new Function(
+                'o',
+                'return "' + (
+                    str
+                    .replace(/["\n\r\u2028\u2029]/g, function($0) {
+                    return rc[$0];
+                    })
+                    .replace(/\{([\s\S]+?)\}/g, '" + o["$1"] + "')
+                ) + '";'
+                );
+        };
+    }());
 
-function request(postalCode) {
-  var protocol = location.protocol !== 'file:' ? location.protocol : 'http:';
-  fetch(protocol + '//stout-brand-finder.herokuapp.com/graphql?query=' + encodeURIComponent(getQuery(postalCode)))
-    .then(function(resp) { return resp.json(); })
-    .then(function (data) {
-      setup(data.data.locations);
-      return data;
-    })
-    .then(function (data) { return exports[MAIN](data); })
-    .catch(function (err) {
-      console.log(err);
-      return err;
-    });
-}
-
-var DEFAULT_LAT_LONG = [35.8278, -78.6421]; // Raleigh, NC
-var MAIN = '_buildThatMap'; // main func. called when google maps js loads
-var SETUP = 'setupFinder'; // setup func. called when location data loads
-
-var mapManager, isReady = false;
-
-function setup(locations) {
-  if (!isReady || typeof google === 'undefined') return setTimeout(function() { setup(locations); }, 10);
-
-  if (!mapManager) {
-    mapManager = new MapManager(locations);
-    mapManager.template(MapManager.INFO_WINDOW, function(data) {
-      return [
-        /*jshint ignore:start*/
-        '<article class="info-window">',
-          '<h3 class="info-store">', data.name, '</h3>',
-          '<span class="info-address">', data.street, '</span>',
-          '<span class="info-city">', data.city, '</span>, ',
-          '<span class="info-state">', data.state, '</span> ',
-          '<span class="info-zip">', data.zip, '</span>',
-        '</article>'
-        /*jshint ignore:end*/
-      ].join('');
-    });
-
-    mapManager.template(MapManager.LOCATIONS_LIST, function(data) {
-      return data.map(function(loc) {
+    function getQuery (postalCode) {
         return [
-          /*jshint ignore:start*/
-          '<div class="location" data-id="', loc._id , '">',
-            '<h3 class="location-store">', loc.name, '</h3>',
-            '<span class="location-address">', loc.address, '</span>',
-            '<span class="location-city">', loc.city, '</span>, ',
-            '<span class="location-state">', loc.state, '</span> ',
-            '<span class="location-zip">', loc.zip, '</span>',
-          '</div>',
-          /*jshint ignore:end*/
+            'query {',
+                // customize the zip based on what the user is looking at
+                'locations(brand:BOSS, zip:' + postalCode + ') {',
+                    // all the fields we care about for our UI
+                    'name street city state zip lat long distance',
+                '}',
+            '}',
         ].join('');
-      }).join('');
-    });
-  }
+    }
 
-  mapManager.draw();
-  mapManager.setLocations(locations);
-}
+    function request(postalCode) {
+        if (finder.insertBefore) {
+            const text = document.createElement('div');
+            text.style = 'margin: 1em auto 3em;';
+            // text.textContent = 'Searching in ' + postalCode + '...';
+            finder.insertBefore(text, finder.firstChild);
+        }
 
-request($('#filter-locations').value);
+        var protocol = location.protocol !== 'file:' ? location.protocol : 'http:';
+        fetch(protocol + '//stout-brand-finder.herokuapp.com/graphql?query=' + encodeURIComponent(getQuery(postalCode)))
+            .then(function(resp) { return resp.json(); })
+            .then(function (data) {
+                draw(data.data.locations);
+                return data;
+            })
+
+						.catch(function (err) {
+                console.log(err);
+                return err;
+            });
+    }
+
+    const finder = document.getElementById('finder-box');
+    const form = document.getElementById('search-form');
+    const input = document.getElementById('zip-field');
+    // form.addEventListener('submit', (event) => {
+    //     event.preventDefault();
+    //     request(input.value);
+    // }, false);
+		form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        request(input.value);
+			}, false);
+
+    const tmpl = [
+      '<div class="location-box">',
+        '<div class="flav-headline">',
+          '<h1>{name}</h1>',
+          '<p class="sub-head">{street} {city} {state} {zip}</p>',
+          '<a href="http://maps.google.com/?q={name} {street} {city} {zip}"><p>link to map</p></a>',
+        '</div>',
+      '</div>'
+    ].join('');
+    const template = makeInterpolator(tmpl);
+
+    // const draw = (locations) => {
+    //     if (locations.length === 0) {
+    //         finder.innerHTML = '<div>Unfortunately our products are not available in this location</div>';
+    //     } else {
+    //         finder.innerHTML = locations.reduce((html, location) => html + template(location), '');
+    //     }
+    // };
+
+		var draw = function draw(locations) {
+    if (locations.length === 0) {
+        finder.innerHTML = '<div>Unfortunately our products are not available in this location</div>';
+    } else {
+        finder.innerHTML = locations.reduce(function (html, location) {
+            return html + template(location);
+        }, '');
+    }
+};
+
+    request(input.value );
+})();
